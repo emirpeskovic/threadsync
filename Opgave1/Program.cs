8925b2e002652dfef8973564a9506d0c6a26ddcc
Emir <me@emir.dk>
2022-05-03 20:34:02 +0200

    Add project files.

---
+++ Opgave1/Program.cs
@@ -0,0 +1,57 @@
+﻿// Variable to increment or decrement
+int count = 0;
+
+// Monitor object
+object obj = new();
+
+// Increment thread
+Thread t1 = new(delegate ()
+{
+    while (true)
+    {
+        // Monitor enter
+        Monitor.Enter(obj);
+
+        // Increment count
+        count += 2;
+
+        // Print count to console
+        Console.WriteLine("Thread 1: count = {0}", count);
+
+        // Monitor exit
+        Monitor.Exit(obj);
+
+        // Sleep for a second
+        Thread.Sleep(1000);
+    }
+});
+
+// Decrement thread
+Thread t2 = new(delegate ()
+{
+    while (true)
+    {
+        // Monitor enter
+        Monitor.Enter(obj);
+
+        // Decrement count
+        count--;
+
+        // Print count to console
+        Console.WriteLine("Thread 2: count = {0}", count);
+
+        // Monitor exit
+        Monitor.Exit(obj);
+
+        // Sleep for a second
+        Thread.Sleep(1000);
+    }
+});
+
+// Start both threads
+t1.Start();
+t2.Start();
+
+// Join threads
+t1.Join();
+t2.Join();
---
+++ Opgave2/Program.cs
@@ -0,0 +1,63 @@
+﻿// Count how many symbols we have printed so far
+int count = 0;
+
+// Monitor object
+object obj = new();
+
+Thread t1 = new(delegate ()
+{
+    while (true)
+    {
+        lock (obj) // Remove for opgave 2, keep in for 3 :-)
+        {
+            // Do this 60 times
+            for (int i = 0; i < 60; i++)
+            {
+                // Print a symbol
+                Console.Write("*");
+
+                // Increment the count
+                count++;
+            }
+
+            // Write total count
+            Console.WriteLine(" {0}", count);
+
+            // Just sleep so the console doesn't get flooded
+            Thread.Sleep(200);
+        }
+    }
+});
+
+Thread t2 = new(delegate ()
+{
+    while (true)
+    { 
+        lock (obj) // Remove for opgave 2, keep in for 3 :-)
+        {
+            // Do this 60 times
+            for (int i = 0; i < 60; i++)
+            {
+                // Print a symbol
+                Console.Write("#");
+
+                // Increment the count
+                count++;
+            }
+
+            // Write total count
+            Console.WriteLine(" {0}", count);
+
+            // Just sleep so the console doesn't get flooded
+            Thread.Sleep(200);
+        }
+    }
+});
+
+// Start the threads
+t1.Start();
+t2.Start();
+
+// Join threads
+t1.Join();
+t2.Join();
